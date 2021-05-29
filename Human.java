0f02d13043f034ea65c7b39bb2352d05535fccf8
Giwang Eka Risti <76390020+wangteam15@users.noreply.github.com>
2021-05-29 12:53:16 +0700

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,6 @@
+public class Human extends LivingThing{
+    @Override
+    public void walk(){
+        System.out.println("Human walks...");
+        }
+}
---
+++ LivingThing.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Trax
+ */
+public abstract class LivingThing {
+    public void breath(){
+        System.out.println("Living Thing breathing...");
+    }
+    public void eat(){
+        System.out.println("Living Thing eating...");
+    }
+    public abstract void walk();
+}
